f48e9dbce9bd41b8aec4c05c5a3e102b66a2d3ce
Martin Krueger <martin.krueger@sabio.de>
2021-01-05 10:42:50 +0100

    initial setup with dummy main

---
+++ cmd/CheckNomadJob.go
@@ -0,0 +1,20 @@
+package cmd
+
+//import nomad "github.com/hashicorp/nomad/api"
+
+func main() {
+	//client := nomad.Client{}
+	//tlsConfig := nomad.TLSConfig{
+	//	CACert:        "",
+	//	CAPath:        "",
+	//	CACertPEM:     nil,
+	//	ClientCert:    "",
+	//	ClientCertPEM: nil,
+	//	ClientKey:     "",
+	//	ClientKeyPEM:  nil,
+	//	TLSServerName: "",
+	//	Insecure:      false,
+	//}
+	//
+	//nomad.ConfigureTLS(client)
+}
---
+++ go.mod
@@ -0,0 +1,1 @@
+module "icinga-check-nomad-job"
