c265ee32a307af7dc3c638bae7cdb2369e33f8e4
Martin Krueger <martin.krueger@sabio.de>
2021-01-20 09:52:50 +0100

    changed test client settings

--- test/test.go
+++ test/test.go
@@ -1,22 +1,13 @@
 package test
 
 import (
-	"bitbucket.org/sabio-it/icinga-check-nomad-job/internal"
 	nomad "github.com/hashicorp/nomad/api"
 	"log"
 	"os"
-	"testing"
 )
 
-func gretaClient() *nomad.Client {
-	tlsConfig := &nomad.TLSConfig{
-		CACert:     "greta/ca.pem",
-		ClientCert: "greta/cert.pem",
-		ClientKey:  "greta/key.pem",
-		Insecure:   false,
-	}
-
-	config := &nomad.Config{Address: "https://nomad01.greta-internal.hc.swops.cloud:4646", TLSConfig: tlsConfig}
+func client() *nomad.Client {
+	config := &nomad.Config{Address: "http://localhost:4646"}
 	client, err := nomad.NewClient(config)
 
 	println(os.Getwd())
@@ -28,18 +19,18 @@ func gretaClient() *nomad.Client {
 	return client
 }
 
-func TestGretaService(t *testing.T) {
-	status := internal.NewServiceCheck(gretaClient(), "internal-nexus").DoCheck()
-
-	if status != 0 {
-		t.Fail()
-	}
-}
-
-func TestGretaServiceUnkown(t *testing.T) {
-	status := internal.NewServiceCheck(gretaClient(), "some-non-existing-job").DoCheck()
-
-	if status != 3 {
-		t.Fail()
-	}
-}
+//func TestGretaService(t *testing.T) {
+//	status := internal.NewServiceCheck(gretaClient(), "internal-nexus").DoCheck()
+//
+//	if status != 0 {
+//		t.Fail()
+//	}
+//}
+//
+//func TestGretaServiceUnkown(t *testing.T) {
+//	status := internal.NewServiceCheck(gretaClient(), "some-non-existing-job").DoCheck()
+//
+//	if status != 3 {
+//		t.Fail()
+//	}
+//}
